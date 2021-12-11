2bf20b05290d0601d5a01d6d3b1d959a16596b5b
Dimitri Prosper <dimitri_prosper@us.ibm.com>
2021-12-11 00:02:00 -0500

    updated to django 4.0

--- app/wsgi.py
+++ app/wsgi.py
@@ -1,10 +1,10 @@
 """
-WSGI config for app project.
+WSGI config for mysite project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
