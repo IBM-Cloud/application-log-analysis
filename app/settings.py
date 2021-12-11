4f814a4deb4b18c1bbd4eb73b1c3dffc280c1cc2
Dimitri Prosper <dimitri_prosper@us.ibm.com>
2021-12-11 00:46:26 -0500

    added CSRF_COOKIE_DOMAIN

--- app/settings.py
+++ app/settings.py
@@ -124,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_COOKIE_DOMAIN='.appdomain.cloud'
